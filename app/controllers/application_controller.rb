16b074d2773938f5135d73f7c5d0c6a673c7d081
Hayato Ise <hayatoise.com@gmail.com>
2021-10-13 17:07:02 +0900

    chore: Rails チュートリアルで不要な問題で詰まらないように Rails 6.0.4.1 で構築し直した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "¡Hola, mundo!"
-    end
-
-    def goodbye
-        render html: "goodbye, world!"
-    end
 end
